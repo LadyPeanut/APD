106e17d2e74f34736196cdd773ad84a23d11924f
Patricia Lazaro <plazaro1994@gmail.com>
2015-12-15 17:32:32 +0100

    Primer commit y demases

---
+++ src/heuristica/SimAnnealingSetCover.java
@@ -0,0 +1,29 @@
+/*
+ * Patricia Lazaro Tello (554309)
+ */
+
+package heuristica;
+
+public class SimAnnealingSetCover {
+	
+	/**
+	 * Clase que implementa la heuristica de Enfriamiento Simulado
+	 * para el problema de Set Cover.
+	 * 
+	 * El problema de Set Cover:
+	 * ENTRADA: S1, S2... Sm (conjuntos), entero k (<= m)
+	 * SALIDA: <true> si existen k conjuntos de los dados
+	 * cuya unión da el mismo resultado que la union de los
+	 * m conjuntos iniciales
+	 * 
+	 * Simulated Annealing:
+	 * ==============================================================
+	 * 1. Inicializar con una solucion aleatoria. T alta.
+	 * BUCLE:
+	 * 2. Movimiento.
+	 * 3. Evaluar la nueva solucion.
+	 * 4. Elegir la nueva solucion.
+	 * 5. Reducir T.
+	 */
+
+}
